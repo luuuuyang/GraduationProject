44b667f1b3456178338b6b5334c4852fb8df07a5
luuuuyang <luuuuyang@163.com>
2020-09-13 16:29:41 +0800

    添加动画状态机，完善角色上下左右移动动画

---
+++ Source/PixelGame/Enums/AnimationState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/UserDefinedEnum.h"
+#include "AnimationState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXELGAME_API UAnimationState : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PixelGame/n.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "n.h"
+
+n::n()
+{
+}
+
+n::~n()
+{
+}
