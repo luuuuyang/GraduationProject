6f332f0e8dd967fc4df0678182a44e23d082b432
luuuuyang <luuuuyang@163.com>
2020-09-16 16:44:50 +0800

    实现角色拾起武器

---
+++ Source/PixelGame/Interfaces/ItemInteractionInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemInteractionInterface.h"
+
+// Add default functionality here for any IItemInteractionInterface functions that are not pure virtual.
