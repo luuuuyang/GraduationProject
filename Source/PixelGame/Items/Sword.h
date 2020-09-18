3cf8a7036af22416fb5591e3fb77f154f0496587
luuuuyang <luuuuyang@163.com>
2020-09-18 20:09:20 +0800

    实现装备技能框，实现拾起武器并出现在UI中

---
+++ Source/PixelGame/Items/Sword.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/PickupItem.h"
+#include "Sword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXELGAME_API ASword : public APickupItem
+{
+	GENERATED_BODY()
+	
+};
