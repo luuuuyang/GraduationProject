5d9104c9b150c3504eecb5448cd62304fe4c230f
luuuuyang <luuuuyang@163.com>
2020-10-09 22:03:26 +0800

    添加AI，AI生命条，AI受到玩家攻击时生命值减少并反应在生命条中

---
+++ Source/PixelGame/AI/PaperAICharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PaperAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXELGAME_API APaperAICharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
