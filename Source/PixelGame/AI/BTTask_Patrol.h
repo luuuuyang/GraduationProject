191f112193e8150df22ca011724e792e85a8e718
luuuuyang <luuuuyang@163.com>
2020-10-13 22:00:11 +0800

    增加AI巡逻行为

---
+++ Source/PixelGame/AI/BTTask_Patrol.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Patrol.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXELGAME_API UBTTask_Patrol : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
